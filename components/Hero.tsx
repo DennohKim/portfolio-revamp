9cafcd11a0d584211de0155f3c855ecb159ae184
Dennis Kimathi <kimathidennis38@gmail.com>
2022-09-28 20:08:13 +0300

    style: complete hero section stylings

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
@@ -27,10 +28,30 @@ const Hero = (props: Props) => {
         height={150}
         className="relative rounded-full mx-auto object-cover"
       />
-      <h1>
-        <span>{text}</span>
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
+      <h1 className="text-5xl lg:text-6xlfont-semibold scroll-px-10">
+        <span className="mr-3 ">{text}</span>
         <Cursor cursorColor="#F7ABBA" />
       </h1>
+      <div className="pt-5">
+        <Link  href="#about">
+        <button className="heroButton">About</button>
+        </Link>
+        <Link href="#experience">
+        <button className="heroButton">Experience</button>
+        </Link>
+        <Link href="#skills">
+        <button className="heroButton">Skills</button>
+        </Link>
+        <Link href="#projects">
+        <button className="heroButton">Projects</button>
+        </Link>
+      
+      </div>
+
+      </div>
+      
     </div>
   );
 };
