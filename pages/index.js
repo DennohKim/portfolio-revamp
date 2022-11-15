c4ce2cef8232e46dc52233440e59825c084f1a74
“dennohkim” <kimathidennis38@gmail.com>
2022-11-15 10:58:14 +0300

    feat: implement project section design layout

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import About from '../components/About'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
 import WorkExperience from '../components/WorkExperience'
 
 export default function Home() {
@@ -37,7 +38,9 @@ export default function Home() {
         <Skills/>
       </section>
 
-      {/* Project */}
+      <section id="projects" className='snap-start'>
+        <Projects />
+      </section >
 
       {/* Contact Me */}
 
