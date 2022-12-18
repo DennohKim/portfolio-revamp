24640d6cedbf6854d3c43bc037803b04cc43e95e
“dennohkim” <kimathidennis38@gmail.com>
2022-12-18 10:24:37 +0300

    fix: change location

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -43,7 +43,7 @@ const ContactMe = (props: Props) => {
 
           <div className="flex items-center space-x-5 justify-center ">
             <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse"  />
-            <p className="text-xl">A call away</p>
+            <p className="text-xl">Nairobi Garage, General Mathenge, Westlands</p>
           </div>
         </div>
 
